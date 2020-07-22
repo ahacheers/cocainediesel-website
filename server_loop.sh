5eedcf74670419d3aeb4ee58ecef610951dc5a75
Michael Savage <mikejsavage@gmail.com>
2020-07-22 17:48:07 +0300

    Make server_loop.sh check for updates

--- server_loop.sh
+++ server_loop.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 # An example script for running a server that updates and restarts itself
-#
-# You still need to manually shutdown the server to start an update, which can
-# be accomplished by typing `quit` in the script window, or with rcon
-#
-# Usage: ./server_loop.sh +set sv_hostname "Best server ever"
 
 while true; do
+	if [ "`curl https://update.cocainediesel.fun/version.txt`" != "`cat version.txt`" ]; then
+		echo quit
+	fi
+	sleep 5m
+done | while true; do
 	./headlessupdater
-	./server "$@"
-	sleep 1
+	./server
+	sleep 5
 done
